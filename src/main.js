eeba03b9fcf12bdb5fb988a33d0a545743038bac
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-02-27 06:34:41 -0500

    Added database stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import './assets/base.css'
 
+//database stuff
+import SQLite from 'tauri-plugin-sql-api';
+console.log(SQLite)
+const db = await SQLite.open('./finances.sqlite');
+
+await db.execute(`
+    INSERT INTO transactions VALUES ('Test', ?1, ?2 )
+`, [29.99, Date.now()]);
+
+export { db };
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-var sqlite3 = require('sqlite3');
-var db;
-
 
 const app = createApp(App)
 
