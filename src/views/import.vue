f403c7682f75dc7aed727645c0cb970e8e52afd9
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-02-27 06:34:01 -0500

    added import page

---
+++ src/views/import.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+
+
+
+</script>
+
+
+<template>
+    <div class="bg-base-200 rounded-box px-5 py-3 flex flex-col w-full">
+        <div>
+            <h1>Import</h1>
+        </div>
+
+        <ul class="steps mt-10">
+            <li class="step step-primary">Upload File</li>
+            <li class="step">Do Something Else</li>
+            <li class="step">Do more stuff</li>
+            <li class="step">Done</li>
+        </ul>
+
+        <div class="mx-auto my-auto flex flex-col">
+            <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+
+            <div class="flex justify-between my-5">
+                <button class="btn btn-neutral">Back</button>
+                <button class="btn btn-primary">Next</button>
+            </div>
+        </div>
+
+    </div>
+</template>
