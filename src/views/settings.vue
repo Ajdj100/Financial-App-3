6dd8d29e3975241fbb3f530d4a0bc09300ce8788
Alistair Campbell <AJCampbell@conestogac.on.ca>
2024-06-02 14:21:01 -0400

    Added some random buttons to settings page

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,7 +1,18 @@
+<script setup>
+import { db } from '@/main';
+
+async function deleteDatabase() {
+    
+}
+
+</script>
+
+
 <template>
     <div class="bg-base-200 rounded-box px-5 py-3 w-full h-full">
         <h1>Settings</h1>
         <div class="py-5">
+            <!-- theme -->
             <div class="flex items-center justify-between max-w-xl">
                 <h3>Theme</h3>
                 <label class="flex cursor-pointer items-center gap-2">
@@ -11,6 +22,11 @@
                 </label>
             </div>
             <div class="divider"></div>
+            <!-- database stuff -->
+            <div class="flex items-center justify-between max-w-xl">
+                <h3>Delete Database (lol I'll implement this later)</h3>
+                <button class="btn btn-outline btn-error">Delete</button>
+            </div>
         </div>
     </div>
 </template>
